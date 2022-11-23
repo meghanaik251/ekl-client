0ce9f771cc1506d025a7cb0d82cea434309739b2
Mounahaadi-dev <mounesh_h@eklakshya.com>
2022-11-23 16:03:24 +0530

    url changed

--- services/constants.js
+++ services/constants.js
@@ -1,9 +1,9 @@
 const productionUrl = "https://eklakshya.com/api/";
-const developmentUrl = "http://localhost:3000/api/";
+const developmentUrl = "http://15.206.151.191/api/";
 
 const mediaUrl = "https://s3.ap-south-1.amazonaws.com/eklakshya.com/";
 
-const isProductionEnvironment = !false;
+const isProductionEnvironment = false;
 
 const apiUrl = isProductionEnvironment ? productionUrl : developmentUrl;
 
