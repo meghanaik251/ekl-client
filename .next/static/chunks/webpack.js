cfca3153ee9d1d53ba8f793be9123fc97fa9e5e1
Mounahaadi-dev <mounesh_h@eklakshya.com>
2022-12-12 14:59:20 +0530

    Uppercase of nav items

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f77028fdb580334f"; }
+/******/ 		__webpack_require__.h = function() { return "2928d7aa58c10811"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
