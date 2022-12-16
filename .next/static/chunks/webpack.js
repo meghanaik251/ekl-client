2310d9ed6d89ea8e67f659424a16dfd62fa5dc3b
Mounahaadi-dev <mounesh_h@eklakshya.com>
2022-12-16 15:39:34 +0530

    3:38 16-12-22 likes I thought that i have pushed the code. But it didn't happen like that. So now doing it

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1d968a612c68e56b"; }
+/******/ 		__webpack_require__.h = function() { return "266750542502f98f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
